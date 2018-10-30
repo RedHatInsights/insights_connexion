c209f75e27554a1c8385589ef23c3ae1f92d6dad
Chris Kyrouac <ckyrouac@redhat.com>
2018-10-30 14:21:05 -0400

    Invalid request if body parse fail in log middleware

--- insights_connexion/app.py
+++ insights_connexion/app.py
@@ -1,6 +1,7 @@
 import asyncio
 import json
 import uuid
+from json import JSONDecodeError
 
 from sqlalchemy.orm.exc import NoResultFound
 
@@ -64,10 +65,17 @@ def _items_to_json(items):
 
 @web.middleware
 async def log_middleware(request, handler):
+    try:
+        req_body = await request.json() if request.has_body else None
+    except(JSONDecodeError) as e:
+        log.error(e)
+        return responses.invalid_request_parameters('Malformed JSON in request body.')
+
     req_id = str(uuid.uuid4())
+
     log.info(json.dumps({'type': 'request',
                          'req_id': req_id,
-                         'body': await request.json() if request.has_body else None,
+                         'body': req_body,
                          'cookies': _items_to_json(request.cookies.items()),
                          'content-type': request.content_type,
                          'content-length': request.content_length,
